3339c3e98191ecb8082f33ecb2d9b05fc406d7aa
SohiebWedyan <153872199+SohiebWedyan@users.noreply.github.com>
2025-07-13 14:46:06 +0300

    Update restaurant_dashboard.py

--- restaurant_dashboard.py
+++ restaurant_dashboard.py
@@ -4,7 +4,8 @@ from firebase_admin import credentials, firestore
 
 # تهيئة Firebase Admin مرة واحدة فقط
 if not firebase_admin._apps:
-    cred = credentials.Certificate("smartserve-multirestaurant-firebase-adminsdk-fbsvc-481204e01b.json")
+    firebase_key = st.secrets["FIREBASE_KEY"]
+    cred = credentials.Certificate(json.loads(firebase_key))
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
